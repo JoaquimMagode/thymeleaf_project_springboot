4050e8c103180e5375a455d2bd752dcb2c401470
Joaquim Magode <135075390+JoaquimMagode@users.noreply.github.com>
2025-12-18 14:10:27 +0530

    add

--- src/main/java/com/thymeleaf/thymeleaf_project/controller/homeController.java
+++ src/main/java/com/thymeleaf/thymeleaf_project/controller/homeController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class homeController {
-    
-    @GetMapping("/")
+
+    @GetMapping({"/", "/home"})
     public String home(Model model) {
-       model.addAttribute("message", "Welcome to the Home Page!");
+        model.addAttribute("message", "Welcome to the Home Page!");
         return "home";
     }
 }
